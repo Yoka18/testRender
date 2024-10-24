bea9e223c5456f6051f3a89d6dd341caa5a78809
inmisin <omeroflu2@gmail.com>
2024-10-25 01:03:26 +0300

    abo

--- server.js
+++ server.js
@@ -12,35 +12,38 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
 app.get('/room', (req, res) => {
-    res.redirect(`/room/${uuidV4()}`)
+  res.redirect(`/room/${uuidV4()}`)
 })
 
 app.get('/room/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
+  res.render('room', { roomId: req.params.room })
 })
 
+const users = {}
+
 io.on('connection', socket => {
-    console.log("Someone joined the server")
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).emit('user-connected', userId)
+  socket.on('join-room', (roomId, userId) => {
+    if (!users[roomId]) users[roomId] = []
+    users[roomId].push(userId)
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
 
-        // Mesajları dinle ve ilgili odaya ilet
-        socket.on('message', (message) => {
-            io.to(roomId).emit('createMessage', message)
-        })
-
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId)
-            console.log("Someone left from the server")
-        })
+    socket.on('disconnect', () => {
+      users[roomId] = users[roomId].filter(id => id !== userId)
+      socket.to(roomId).emit('user-disconnected', userId)
     })
+
+    // WebRTC sinyalleme mesajlarını iletme
+    socket.on('message', message => {
+      socket.to(roomId).emit('message', message)
+    })
+  })
 })
 
 server.listen(3000, () => {
-    console.log("Server started on port 3000")
+  console.log("Server started on port 3000")
 })
